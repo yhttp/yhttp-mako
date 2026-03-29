6346f936d34cf78fe79fb7466b94ca53bebd2479
pylover <vahid.mardani@gmail.com>
2026-03-29 23:21:56 +0400

    Feature: `mako.install(..., data=None)` as `app.template_globals` and `data` keyword argument for the `app.template` decorator: `@app.template(..., data=None)`.

--- yhttp/ext/mako/install.py
+++ yhttp/ext/mako/install.py
@@ -11,13 +11,16 @@ lookup: templates
 '''
 
 
-def decoratorfactory(template):
+def decoratorfactory(template, data=None):
     def decorator(handler):
         @functools.wraps(handler)
         def wrapper(req, *a, **kw):
             data = handler(req, *a, **kw)
-            t = req.application.lookup.get_template(template)
+            app = req.application
+            data.update(app.template_globals)
+            t = app.lookup.get_template(template)
             req.response.type = 'text/html'
+
             if hasattr(req, 'translator'):
                 data['_'] = req.translator.gettext
                 data['N_'] = req.translator.ngettext
@@ -34,11 +37,12 @@ def decoratorfactory(template):
     return decorator
 
 
-def install(app):
+def install(app, data=None):
     app.template = decoratorfactory
     app.settings.merge('mako: {}')
     app.settings['mako'].merge(DEFAULT_SETTINGS)
     app.template = decoratorfactory
+    app.template_globals = data if data else {}
 
     @app.when
     def ready(app):
